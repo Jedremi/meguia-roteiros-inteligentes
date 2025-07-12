e007cdbccfa0c26c52341c2382bdff98bc2b1d83
Jedremi <69273603+Jedremi@users.noreply.github.com>
2025-07-12 09:00:03 -0300

    Add .next/static/chunks/src_app_perfil_page_tsx_030333c4._.js 1752321602643

---
+++ .next/static/chunks/src_app_perfil_page_tsx_030333c4._.js
@@ -0,0 +1,14 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_@firebase_util_dist_2199a7ac._.js",
+  "static/chunks/node_modules_@firebase_auth_dist_esm2017_d0a1459b._.js",
+  "static/chunks/node_modules_@firebase_webchannel-wrapper_dist_20785bb9._.js",
+  "static/chunks/node_modules_@firebase_firestore_dist_index_esm2017_c2fcaa2e.js",
+  "static/chunks/node_modules_@firebase_firestore_dist_index_cjs_75b7d56d.js",
+  "static/chunks/node_modules_b19f9855._.js",
+  "static/chunks/src_dfc2743b._.js"
+],
+    source: "dynamic"
+});
